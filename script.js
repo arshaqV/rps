cfe4a4c856dc4198383fd06aba6d73412b6afb35
arshaqV <arshaq.assoo@gmail.com>
2024-02-18 20:51:10 +0530

    Add game end message + ability to restart

--- script.js
+++ script.js
@@ -18,29 +18,29 @@ function rockPaperScissors(playerSelection, computerSelection) {
   else if (playerSelection.toUpperCase() == "ROCK") {
     if (computerSelection == "Paper") {
       compScore++
-      return "You Lose! Paper beats Rock"
+      return "Rock beaten by Paper..."
     }
     if (computerSelection == "Scissors") {
       userScore++
-      return "You Win! Rock beats Scissors"
+      return "Rock beats Scissors!"
     }
   } else if (playerSelection.toUpperCase() == "SCISSORS") {
     if (computerSelection == "Paper") {
       userScore++
-      return "You Win! Scissors beats Paper"
+      return "Scissors beats Paper!"
     }
     if (computerSelection == "Rock") {
       compScore++
-      return "You Lose! Rock beats Scissors"
+      return "Scissors beaten by Rock..."
     }
   } else if (playerSelection.toUpperCase() == "PAPER") {
     if (computerSelection == "Rock") {
       userScore++
-      return "You Win! Paper beats Rock"
+      return "Paper beats Rock!"
     }
     if (computerSelection == "Scissors") {
       compScore++
-      return "You Lose! Scissors beats Paper"
+      return "Paper beaten by Scissors..."
     }
   } else return "Invalid"
 }
@@ -49,13 +49,24 @@ function game() {
   userScoreboard.innerHTML = userScore
   compScoreboard.innerHTML = compScore
   if (userScore == 5 || compScore == 5) {
-    alert((userScore == 5 ? "Player" : "Computer") + " is the winner!")
+    let endMessage
+    if (userScore == 5) {
+      endMessage = document.createElement("div")
+      endMessage.id = "victory"
+      endMessage.textContent = "YOU WIN!"
+    } else {
+      endMessage = document.createElement("div")
+      endMessage.id = "loss"
+      endMessage.textContent = "YOU LOSE..."
+    }
+    results.insertBefore(endMessage, results.firstChild)
     gameOver = true
   }
 }
 
 const actionButtons = document.getElementsByClassName("actionButton")
-const results = document.getElementById("results")
+const retryButton = document.getElementById("retry")
+const results = document.getElementById("moveHistory")
 console.log(actionButtons)
 
 const handleAction = (event) => {
@@ -70,6 +81,16 @@ const handleAction = (event) => {
   results.appendChild(linebreak)
   game()
 }
+
+const handleRetry = (event) => {
+  userScore = 0
+  compScore = 0
+  results.innerHTML = ""
+  gameOver = false
+  game()
+}
+
+retryButton.addEventListener("click", handleRetry)
 
 for (let i = 0; i < actionButtons.length; i++) {
   actionButtons[i].addEventListener("click", handleAction)
